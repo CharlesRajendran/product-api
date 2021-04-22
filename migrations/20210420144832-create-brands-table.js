7724afc8edecd920ebe96cf5b20edd592a40b788
charlesrajendran <charlesrajendran44@gmail.com>
2021-04-23 03:50:46 +0530

    feat: update migrations to create and populate product and brand table data

---
+++ migrations/20210420144832-create-brands-table.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-unused-vars */
+const tableName = 'brands';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable(tableName, {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      name: {
+        type: Sequelize.STRING(100),
+        allowNull: false,
+      },
+      address: {
+        type: Sequelize.STRING(1000),
+      },
+      city: {
+        type: Sequelize.STRING(100),
+      },
+      country: {
+        type: Sequelize.STRING(100),
+      },
+      zip_code: {
+        type: Sequelize.STRING(100),
+      },
+      country_code: {
+        type: Sequelize.STRING(100),
+      },
+      reference: {
+        type: Sequelize.STRING(100),
+      },
+      website: {
+        type: Sequelize.STRING(1000),
+      },
+      email: {
+        type: Sequelize.STRING(1000),
+        allowNull: false,
+      },
+      phone: {
+        type: Sequelize.STRING(1000),
+      },
+      bank_account: {
+        type: Sequelize.STRING(1000),
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+      },
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable(tableName);
+  },
+};
--- migrations/20210422204156-create-products-table.js
+++ migrations/20210422204156-create-products-table.js
@@ -7,45 +7,49 @@ module.exports = {
       id: {
         type: Sequelize.INTEGER,
         primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
       },
       name: {
         type: Sequelize.STRING(100),
-        allowNull: false
+        allowNull: false,
       },
       slug: {
         type: Sequelize.STRING(100),
         allowNull: false,
-        unique: true
+        unique: true,
       },
       sku: {
         type: Sequelize.STRING(12),
         allowNull: false,
-        unique: true
+        unique: true,
       },
       brand: {
-        type: Sequelize.STRING(100),
-        allowNull: false
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'brands',
+          key: 'id',
+          allowNull: false,
+        },
       },
       image: {
-        type: Sequelize.STRING(1000)
+        type: Sequelize.STRING(1000),
       },
       unit: {
-        type: Sequelize.STRING(20)
+        type: Sequelize.STRING(20),
       },
       unit_price: {
-        type: Sequelize.FLOAT
+        type: Sequelize.FLOAT,
       },
       createdAt: {
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
       },
       updatedAt: {
-        type: Sequelize.DATE
-      }
+        type: Sequelize.DATE,
+      },
     });
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.dropTable(tableName);
-  }
+  },
 };
