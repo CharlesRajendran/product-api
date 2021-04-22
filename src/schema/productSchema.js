6c184c9800f4fd86ae5ca68abc20dac0e4a1ae31
charlesrajendran <charlesrajendran44@gmail.com>
2021-04-23 01:51:58 +0530

    feat: enable pagination and order by options to the api

--- src/schema/productSchema.js
+++ src/schema/productSchema.js
@@ -1,9 +1,12 @@
 const Joi = require('joi');
 
 const fetchAllProducts = () => Joi.object().keys({
-  cacheKey: Joi.string()
+  cacheKey: Joi.string(),
+  page: Joi.number().allow(null),
+  limit: Joi.number().allow(null),
+  sortBy: Joi.string().allow(null),
 });
 
 module.exports = {
-  fetchAllProducts
+  fetchAllProducts,
 };
--- src/services/productService.js
+++ src/services/productService.js
@@ -2,15 +2,32 @@
 const Products = require('../models/Product');
 
 const fetchAllProducts = async (attributes) => {
+  const {
+    cacheKey, page, limit, sortBy
+  } = attributes;
+
+  // prepare conditions
+  const options = {};
+  if (limit) {
+    options.offset = page ? page * limit : 0;
+    options.limit = limit;
+  }
+
+  if (sortBy) {
+    options.order = [[sortBy, 'ASC']];
+  }
+
+  console.log('CHDLLK: ', options);
+
   // fetch list of products
-  const products = await Products.findAll();
+  const products = await Products.findAll(options);
 
   return {
     products,
-    cacheKey: attributes.cacheKey
+    cacheKey,
   };
 };
 
 module.exports = {
-  fetchAllProducts
+  fetchAllProducts,
 };
