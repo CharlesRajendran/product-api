29fd769c0466d7c62ff892f3fa77cd9044020b81
charlesrajendran <charlesrajendran44@gmail.com>
2021-04-26 01:47:55 +0530

    test: write test for update and delete product apis

--- test/integration/api/productController.test.js
+++ test/integration/api/productController.test.js
@@ -76,5 +76,112 @@ describe('#ProductAPI', () => {
 
       createdRecordId = response.body.data.id;
     });
+
+    it('should send validation error if new product api is called with wrong data', async () => {
+      const response = await app
+        .post('/')
+        .send({
+          name: 'Sh', // min length should be 3
+          brand: 'Reebok',
+          image: null,
+          unit: 'pcs',
+          unit_price: 198,
+        })
+        .set('Accept', 'application/json');
+
+      expect(response.statusCode).toBe(422);
+    });
+  });
+
+  describe('##PATCH / -> Update Products', () => {
+    let recordId;
+    beforeAll(async () => {
+      // Create Product for Update
+      const response = await Products.create({
+        name: 'Dummay Product',
+        brand: 1,
+        slug: 'Dummay-Product-989',
+        sku: 'Du-894-Th',
+      });
+
+      recordId = response.toJSON().id;
+    });
+
+    afterAll(async () => {
+      // Delete Created Product
+      await Products.destroy({
+        where: {
+          id: recordId,
+        },
+      });
+    });
+
+    it('should update product when passing correct request body', async () => {
+      const response = await app
+        .patch(`/${recordId}`)
+        .send({
+          unit_price: 2000,
+        })
+        .set('Accept', 'application/json');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.status).toBe('success');
+    });
+
+    it('should send `not found error` if update request made to a none existing product', async () => {
+      const response = await app
+        .patch('/1020')
+        .send({
+          unit_price: 1989,
+        })
+        .set('Accept', 'application/json');
+
+      expect(response.statusCode).toBe(404);
+    });
+
+    it('should send `validation error` if update request with wrong request parameters', async () => {
+      const response = await app
+        .patch('/1020')
+        .send({
+          sku: '43009440390-493-040943', // length 8 - 12
+        })
+        .set('Accept', 'application/json');
+
+      expect(response.statusCode).toBe(422);
+    });
+  });
+
+  describe('##DELETE / -> Update Products', () => {
+    let recordId;
+    beforeAll(async () => {
+      // Create Product for Update
+      const response = await Products.create({
+        name: 'Dummay Product',
+        brand: 1,
+        slug: 'Dummay-Product-989',
+        sku: 'Du-894-Th',
+      });
+
+      recordId = response.toJSON().id;
+    });
+
+    it('should delete product when passing correct product id', async () => {
+      const response = await app
+        .delete(`/${recordId}`)
+        .send()
+        .set('Accept', 'application/json');
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.status).toBe('success');
+    });
+
+    it('should send `not found error` if update request made to a none existing product', async () => {
+      const response = await app
+        .delete('/1020')
+        .send()
+        .set('Accept', 'application/json');
+
+      expect(response.statusCode).toBe(404);
+    });
   });
 });
