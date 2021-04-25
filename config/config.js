87e44a836c609d8f159710a9da72c0977c500d8b
charlesrajendran <charlesrajendran44@gmail.com>
2021-04-26 03:31:08 +0530

    refactor: remove secrets in code and replaced with env variables

--- config/config.js
+++ config/config.js
@@ -1,17 +1,16 @@
 module.exports = {
   development: {
-    username: 'root',
-    password: 'root',
-    database: 'ProductDB',
-    host: 'db',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
   test: {
-    username: 'root',
-    password: 'root',
-    database: 'ProductTestDB',
-    port: 4306,
-    host: '0.0.0.0',
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
     dialect: 'mysql',
   },
 };
